9a1b1ad5034c8442b02d4334e202d47bcafc8ec9
mikechao <mike.chao.one@gmail.com>
2024-12-14 10:00:42 -0800

    Laying out text section

---
+++ components/ChatWindow.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+function sendQuestion() {
+    console.log("Send question")
+}
+</script>
+
+<template>
+    <div>
+      <div class="flex rounded-md mt-4">
+        <div class="relative flex flex-col flex-grow items-stretch">
+          <div>
+            <div class="flex shadow-sm mb-4">
+                <textarea
+                 id="chat" rows="1"
+                 placeholder="Send a message"/>
+              <AwesomeButton size="md" @click="sendQuestion"/>
+            </div>
+          </div>
+          <!-- chat back and forth goes here-->
+        </div>
+      </div>
+    </div>
+</template>
--- pages/text.vue
+++ pages/text.vue
@@ -1,15 +1,15 @@
 <template>
   <LayoutPageWrapper class="flex-1 flex">
-    <LayoutPageSection class="flex-1 flex flex-col">
+    <LayoutPageSection class="flex-1 flex flex-col mr-2">
       <h1>Enter the text you would like to ask questions about.</h1>
-      <section>
+      <section class="mt-2">
         <textarea
-        rows="20" 
+          rows="20"
         />
       </section>
     </LayoutPageSection>
     <LayoutPageSection class="flex-1 flex">
-        <h1>The Chat window goes here</h1>
+        <ChatWindow />
     </LayoutPageSection>
   </LayoutPageWrapper>
 </template>
