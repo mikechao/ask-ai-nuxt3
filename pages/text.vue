8e2f880d448dc12ee73790633eea221a560cc869
mikechao <mike.chao.one@gmail.com>
2024-12-13 17:12:11 -0800

    Laying out text section

--- pages/text.vue
+++ pages/text.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>This is the text page</h1>
+  <LayoutPageWrapper class="flex-1 flex">
+    <LayoutPageSection class="flex-1 flex flex-col">
+      <h1>Enter the text you would like to ask questions about.</h1>
+      <section>
+        <textarea
+        rows="20" 
+        />
+      </section>
+    </LayoutPageSection>
+    <LayoutPageSection class="flex-1 flex">
+        <h1>The Chat window goes here</h1>
+    </LayoutPageSection>
+  </LayoutPageWrapper>
 </template>
