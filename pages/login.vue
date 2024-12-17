989b2a52d3a9d4aab4eb1d777c06316200534e11
mikechao <mike.chao.one@gmail.com>
2024-12-16 17:09:57 -0800

    Login with Google pop up

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import { GoogleAuthProvider, signInWithPopup, type Auth } from 'firebase/auth';
+const googleProvider = new GoogleAuthProvider()
+
 const { awesome } = useAppConfig()
+const firebaseAuth = useFirebaseAuth() as Auth
+
+const router = useRouter()
 
 function login() {
     const menus = awesome.layout?.page?.navbar?.menus
@@ -9,11 +15,35 @@ function login() {
         menus[index].to = '/logout'
     }
 }
+
+function loginWithGoogle() {
+    signInWithPopup(firebaseAuth, googleProvider)
+    .then((result) => {
+        console.log('login success')
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        console.log('token', token)
+        // The signed-in user info.
+        const user = result.user;
+        console.log('user', user)
+        router.push('/')
+    }).catch((error) => {
+        console.log('Failed to login')
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('errorCode', errorCode)
+        console.log('errorMessage', errorMessage)
+    })
+}
 </script>
 
 <template>
     <div>
         <h1>This is the login page</h1>
         <AwesomeButton text="Login" @click="login"/>
+        <AwesomeButton @click="loginWithGoogle">
+            <Icon name="simple-line-icons:social-google" class="w-5 h-5 mr-1"/>
+            Login with Google
+        </AwesomeButton>
     </div>
 </template>
