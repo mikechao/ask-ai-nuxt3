6f9524eaa8ba265cf5b62b4fc066f122c8ce61e2
mikechao <mike.chao.one@gmail.com>
2024-12-20 18:07:29 -0800

    Starting to layout audio page

--- pages/audio.vue
+++ pages/audio.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>This is the audio page</h1>
+    <LayoutPageWrapper class="flex-1 flex">
+      <LayoutPageSection class="flex-1 flex flex-col mr-2 h-full">
+        <h3>Choose audio that has some speech that you would like to ask questions about.</h3>
+        <section class="my-4">
+          <AudioFileUploader file-type="audio/*"/>
+        </section>
+      </LayoutPageSection>
+      <LayoutPageSection class="flex-1 flex h-full">
+        <TextChatWindow class="block w-full"/>
+      </LayoutPageSection>
+    </LayoutPageWrapper>
 </template>
