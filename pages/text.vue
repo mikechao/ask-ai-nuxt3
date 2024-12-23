a4f565ed488dc11a99449d94e788a56e89fa3274
mikechao <mike.chao.one@gmail.com>
2024-12-22 16:09:06 -0800

    Observe the height of textarea

--- pages/text.vue
+++ pages/text.vue
@@ -2,11 +2,24 @@
 import TextChatWindow from '~/components/TextChatWindow.vue'
 
 const textChatStore = useTextChatStore()
+
+function observeHeight() {
+  const resizeObserver = new ResizeObserver(function(textAreaEl) {
+    console.log('textAreaEl', textAreaEl)
+    console.log("Size changed");
+  });
+  const textAreaEl = document.getElementById('textArea') as Element
+  resizeObserver.observe(textAreaEl);
+}
+
+onMounted(() => {
+  observeHeight()
+})
 </script>
 
 <template>
   <LayoutPageWrapper class="flex-1 flex">
-    <LayoutPageSection class="flex-1 flex flex-col mr-2 h-full">
+    <div id="textArea" class="flex-1 flex flex-col mr-2 h-full">
       <h1>Enter the text you would like to ask questions about.</h1>
       <section class="mt-1 flex-1 flex">
         <textarea
@@ -14,9 +27,9 @@ const textChatStore = useTextChatStore()
           class="w-full h-full"
         />
       </section>
-    </LayoutPageSection>
-    <LayoutPageSection class="flex-1 flex h-full">
+    </div>
+    <div class="flex-1 flex h-full">
         <TextChatWindow class="block w-full h-full"/>
-    </LayoutPageSection>
+    </div>
   </LayoutPageWrapper>
 </template>
