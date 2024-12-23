334413b995d77435a8dfc1ca4c0b123d4db68ba0
mikechao <mike.chao.one@gmail.com>
2024-12-23 15:22:17 -0800

    Trying to use prompts for image, no memory

--- server/api/image/chat.post.ts
+++ server/api/image/chat.post.ts
@@ -1,16 +1,34 @@
 import { BufferMemory } from "langchain/memory";
 import { getFirestoreChatMessageHistory } from "../../utils/firestoreChatHistory";
 import { ChatOpenAI } from "@langchain/openai"
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatPromptTemplate, HumanMessagePromptTemplate, ImagePromptTemplate, PipelinePromptTemplate, PromptTemplate } from "@langchain/core/prompts";
 import { ConversationChain } from "langchain/chains"
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 
 const runtimeConfig = useRuntimeConfig()
+
+async function getPrompt(imageBase64: string, question: string) {
+  if (imageBase64) {
+    return ChatPromptTemplate.fromMessages([
+      new SystemMessage(`Given the image, you will answer user questions about it.
+        You will take into account the current converstation: {chat_history}`),
+      new HumanMessage(question),
+      new HumanMessage([{image_url: `data:image/jpeg;base64,${imageBase64}`}])
+    ])
+  } else {
+    return ChatPromptTemplate.fromMessages([
+      new SystemMessage(`You will continue answering user questions taking into account the
+        current conversation: {chat_history}`),
+      new HumanMessage(question)
+    ])
+  }
+}
 
 export default defineEventHandler(async event => {
   const body = await readBody(event)
   const token = getCookie(event, 'token') as string
 
-  const { imageBase64, question} = body as ImageChatRequest
+  const { imageBase64, question } = body as ImageChatRequest
 
   const chatHistory = getFirestoreChatMessageHistory(token)
   const memory = new BufferMemory({
@@ -24,20 +42,15 @@ export default defineEventHandler(async event => {
     apiKey: runtimeConfig.openaiAPIKey
   })
 
-  const prompt = ChatPromptTemplate.fromMessages([
-    ["system", "Describe the image provided"],
-    [
-      "user",
-      [{ type: "image_url", image_url: "data:image/jpeg;base64,{base64}" }],
-    ],
-  ])
-  
-  const chain = prompt.pipe(llm)
-  const res = await chain.invoke({ base64: imageBase64 })
+  const prompt = await getPrompt(imageBase64 as string, question)
+
+  const chain = new ConversationChain({ llm: llm, prompt: prompt, memory: memory, verbose: true})
+
+  const res = await chain.call({ question: question})
 
   console.log('res', res)
 
   return {
-    gptResponse: res.content
+    gptResponse: res.response
   }
 })
