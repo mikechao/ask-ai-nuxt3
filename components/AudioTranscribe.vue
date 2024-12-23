cdededd29af09ef029d66c55ae3ac19dec7a68ce
mikechao <mike.chao.one@gmail.com>
2024-12-22 17:46:18 -0800

    Changed displaying audio transcript to textarea

--- components/AudioTranscribe.vue
+++ components/AudioTranscribe.vue
@@ -6,14 +6,16 @@ const disableTranscribe = computed(() => hasFile.value || audioChatStore.isTrans
 <template>
   <div>
     <h3 class="mb-4">Transcribe the speech to text:</h3>
-    <div  class="flex flex-col items-start space-x-4 md:justify-start">
+    <div class="flex flex-col items-start space-x-4 md:justify-start">
       <AwesomeButton text="Transcribe" size="md" :disabled="disableTranscribe" @click="audioChatStore.transcribeFile()"/>
     </div>
   </div>
-  <div
+  <div v-if="audioChatStore.transcript.length > 0" class="mt-4">
+    <textarea 
     v-if="audioChatStore.transcript.length > 0"
-    class="block w-full rounded-md border-0 bg-blue-900 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:p-4 sm:text-sm sm:leading-6 text-sm my-4"
-  >
-    {{ audioChatStore.transcript }}
+    v-model="audioChatStore.transcript"
+    readonly
+    style="overflow:auto;resize:none"
+    />
   </div>
 </template>
