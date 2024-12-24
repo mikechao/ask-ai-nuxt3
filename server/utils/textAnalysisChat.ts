079ae55ea04aa1008746e1ba1f05b56a801d833f
mikechao <mike.chao.one@gmail.com>
2024-12-24 10:57:27 -0800

    Use a custom callback to get token usage

--- server/utils/textAnalysisChat.ts
+++ server/utils/textAnalysisChat.ts
@@ -3,8 +3,15 @@ import { ChatOpenAI } from "@langchain/openai"
 import { BufferMemory } from "langchain/memory";
 import { ConversationChain } from "langchain/chains"
 import { getFirestoreChatMessageHistory } from "./firestoreChatHistory";
+import type { LLMResult } from "@langchain/core/outputs";
 
 const runtimeConfig = useRuntimeConfig()
+
+const llm = new ChatOpenAI({
+  model: 'gpt-4o-mini',
+  temperature: 0,
+  apiKey: runtimeConfig.openaiAPIKey,
+})
 
 export async function textAnalysis(inputs: string[], uid: string, prompt: PromptTemplate): Promise<TextChatResposne> {
   const chatHistory = getFirestoreChatMessageHistory(uid)
@@ -15,24 +22,18 @@ export async function textAnalysis(inputs: string[], uid: string, prompt: Prompt
   });
 
   let totalTokens = 0
-  const llm = new ChatOpenAI({
-    model: 'gpt-4o-mini',
-    temperature: 0,
-    apiKey: runtimeConfig.openaiAPIKey,
-    callbacks: [
-      {
-        handleLLMEnd(output) {
-          // console.log(JSON.stringify(output, null, 2))
-          // tokenUsage.totalTokens is specific to the model gpt-4o-mini
-          totalTokens = output.llmOutput?.tokenUsage.totalTokens
-        },
-      },
-    ],
-  })
+
+  const llmEndHandler = {
+    handleLLMEnd(output: LLMResult) {
+      // console.log(JSON.stringify(output, null, 2))
+      // tokenUsage.totalTokens is specific to the model gpt-4o-mini
+      totalTokens = output.llmOutput?.tokenUsage.totalTokens
+    },
+  }
 
   const chain = new ConversationChain({ llm: llm, prompt: prompt, memory: memory, })
   const joined = inputs.join('\n')
-  const res = await chain.call({ input: joined })
+  const res = await chain.call({ input: joined }, { callbacks: [llmEndHandler]})
 
   return new Promise<TextChatResposne>((resolve) => {
     resolve({
