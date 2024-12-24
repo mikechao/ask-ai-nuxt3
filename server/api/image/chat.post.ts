7c1db7da23f609709ee4577b307fa0ae903e1437
mikechao <mike.chao.one@gmail.com>
2024-12-24 13:55:12 -0800

    Trying to integrate image description into chat history to ask questions about it

--- server/api/image/chat.post.ts
+++ server/api/image/chat.post.ts
@@ -7,6 +7,12 @@ import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 
 const runtimeConfig = useRuntimeConfig()
 
+const llm = new ChatOpenAI({
+  model: 'gpt-4o-mini',
+  temperature: 0,
+  apiKey: runtimeConfig.openaiAPIKey
+})
+
 async function getPrompt(imageBase64: string, question: string) {
   if (imageBase64) {
     return ChatPromptTemplate.fromMessages([
@@ -24,6 +30,35 @@ async function getPrompt(imageBase64: string, question: string) {
   }
 }
 
+function getQuestionPrompt(question: string) {
+  return ChatPromptTemplate.fromMessages([
+    new SystemMessage(`You will continue answering user questions about the image described after "The image:" 
+      given the current conversation: {chat_history}`),
+    new HumanMessage(question)
+  ])
+}
+
+function getImageDescriptionPrompt(imageBase64: string) {
+  return ChatPromptTemplate.fromMessages([
+    new SystemMessage(`Give the image provided by the user, you will provide a detailed 
+      description of the image, include the following aspects:
+      -What is the overall scene or setting?
+      -Who or what is in the image?
+      -What objects, actions or notable features are present?
+      -Are there any specific details regarding the environment?
+      -What emotions or themes does the image convey?
+      Prefix your response with "The image:"`),
+      new HumanMessage({content: [{type: "image_url", image_url: { url: `data:image/jpeg;base64,${imageBase64}`} }]})
+  ])
+}
+
+async function handleImage(imageBase64: string, memory: BufferMemory) {
+  const prompt = getImageDescriptionPrompt(imageBase64)
+  const chain = new ConversationChain({ llm: llm, prompt: prompt, memory: memory, verbose: false})
+  const res = await chain.call({ input: 'Describe the image'})
+  console.log('handlImage res', res)
+}
+
 export default defineEventHandler(async event => {
   const body = await readBody(event)
   const token = getCookie(event, 'token') as string
@@ -36,15 +71,12 @@ export default defineEventHandler(async event => {
       memoryKey: "chat_history"
   });
 
-  const llm = new ChatOpenAI({
-    model: 'gpt-4o-mini',
-    temperature: 0,
-    apiKey: runtimeConfig.openaiAPIKey
-  })
+  if (imageBase64) {
+    await handleImage(imageBase64, memory)
+  }
+  const prompt = getQuestionPrompt(question)
 
-  const prompt = await getPrompt(imageBase64 as string, question)
-
-  const chain = new ConversationChain({ llm: llm, prompt: prompt, memory: memory, verbose: false})
+  const chain = new ConversationChain({ llm: llm, prompt: prompt, memory: memory, verbose: true})
 
   const res = await chain.call({ question: question})
 
