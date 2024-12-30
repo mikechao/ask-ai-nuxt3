42d2d7c7fb18e15487d08998e3b83d46c3acdf6e
mikechao <mike.chao.one@gmail.com>
2024-12-30 12:02:28 -0800

    Try readyBody again

--- server/api/audio/transcribe.post.ts
+++ server/api/audio/transcribe.post.ts
@@ -57,51 +57,12 @@ async function parseWithMulter(event: H3Event<EventHandlerRequest>) {
 
 async function parseForNetlify(event) {
   console.log('parseForNetlify called')
-  const results = await new Promise((resolve) => {
-    // we'll store all form fields inside of this
-    const fields = {};
-
-    // let's instantiate our busboy instance!
-    const bb = busboy({
-      // it uses request headers
-      // to extract the form boundary value (the ----WebKitFormBoundary thing)
-      headers: event.headers
-    });
-
-    // before parsing anything, we need to set up some handlers.
-    // whenever busboy comes across a file ...
-    bb.on(
-      "file",
-      (fieldname, filestream, filename, transferEncoding, mimeType) => {
-        // ... we take a look at the file's data ...
-        filestream.on("data", (data) => {
-          // ... and write the file's name, type and content into `fields`.
-          fields[fieldname] = {
-            filename,
-            type: mimeType,
-            content: data,
-          };
-        });
-      }
-    );
-
-    // whenever busboy comes across a normal field ...
-    bb.on("field", (fieldName, value) => {
-      // ... we write its value into `fields`.
-      fields[fieldName] = value;
-    });
-
-    // once busboy is finished, we resolve the promise with the resulted fields.
-    bb.on("finish", () => {
-      resolve(fields)
-    });
-
-    // now that all handlers are set up, we can finally start processing our request!
-    bb.write(event.body);
-  });
-  console.log('results', results)
-  console.log('results.file', results.file)
-  return results.file.content
+  const eventBody = await readBody(event)
+  const uint8Array = Buffer.from(eventBody, 'base64')
+  console.log('uint8Array', Object.prototype.toString.call(uint8Array))
+  const buffer = Buffer.from(uint8Array)
+  console.log('buffer', Object.prototype.toString.call(buffer))
+  return buffer
 }
 
 export default defineEventHandler(async event => {
@@ -110,7 +71,7 @@ export default defineEventHandler(async event => {
   if (!event.node.req?.body) {
     audioFile = await parseWithMulter(event)
   } else {
-    audioFile = await parseForNetlify(event.node.req)
+    audioFile = await parseForNetlify(event)
   }
   const results = await callDeepgram(audioFile.buffer)
   return results
