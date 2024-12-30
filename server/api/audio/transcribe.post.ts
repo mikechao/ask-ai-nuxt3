162ad201fdb1cfb220c5116198ac3ab37d8420be
mikechao <mike.chao.one@gmail.com>
2024-12-30 11:30:38 -0800

    Use busboy and example from netlify website

--- server/api/audio/transcribe.post.ts
+++ server/api/audio/transcribe.post.ts
@@ -2,6 +2,7 @@ import type { FileSource } from '@deepgram/sdk'
 import { createClient } from '@deepgram/sdk'
 import multer from 'multer'
 import type { H3Event, EventHandlerRequest } from 'h3'
+import * as Busboy from "busboy"
 
 const runtimeConfig = useRuntimeConfig()
 const deepgram = createClient(runtimeConfig.deepgramAPIKey)
@@ -56,12 +57,51 @@ async function parseWithMulter(event: H3Event<EventHandlerRequest>) {
 
 async function parseForNetlify(event) {
   console.log('parseForNetlify called')
-  const eventBody = await readBody(event)
-  console.log('eventBody', Object.prototype.toString.call(eventBody))
-  const result = Buffer.from(eventBody, 'base64')
-  console.log('result === null', result === null)
-  console.log('result', Object.prototype.toString.call(result))
-  return result
+  const results = await new Promise((resolve) => {
+    // we'll store all form fields inside of this
+    const fields = {};
+
+    // let's instantiate our busboy instance!
+    const busboy = new Busboy({
+      // it uses request headers
+      // to extract the form boundary value (the ----WebKitFormBoundary thing)
+      headers: event.headers
+    });
+
+    // before parsing anything, we need to set up some handlers.
+    // whenever busboy comes across a file ...
+    busboy.on(
+      "file",
+      (fieldname, filestream, filename, transferEncoding, mimeType) => {
+        // ... we take a look at the file's data ...
+        filestream.on("data", (data) => {
+          // ... and write the file's name, type and content into `fields`.
+          fields[fieldname] = {
+            filename,
+            type: mimeType,
+            content: data,
+          };
+        });
+      }
+    );
+
+    // whenever busboy comes across a normal field ...
+    busboy.on("field", (fieldName, value) => {
+      // ... we write its value into `fields`.
+      fields[fieldName] = value;
+    });
+
+    // once busboy is finished, we resolve the promise with the resulted fields.
+    busboy.on("finish", () => {
+      resolve(fields)
+    });
+
+    // now that all handlers are set up, we can finally start processing our request!
+    busboy.write(event.body);
+  });
+  console.log('results', results)
+  console.log('results.file', results.file)
+  return results.file.content
 }
 
 export default defineEventHandler(async event => {
