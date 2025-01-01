c7cb302e82f5c155e58a0de96588a74b6e753dfb
mikechao <mike.chao.one@gmail.com>
2025-01-01 11:57:01 -0800

    Increase the main content size

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="overflow-x-hidden">
       <LayoutPageNavbar class="h-[52px] max-h-[52px]" />
-      <LayoutPageContent class="max-w-5xl m-auto h-full">
+      <LayoutPageContent class="max-w-[68rem] m-auto h-full">
         <slot />
       </LayoutPageContent>
       <LayoutPageFooter class="h-[52px] md:h-[42px]" />
