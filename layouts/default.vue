073ade8758939cb9fab63bc5e005367395f3cd7a
mikechao <mike.chao.one@gmail.com>
2024-12-31 16:03:26 -0800

    Shrink the NavBar height a bit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="overflow-x-hidden">
-      <LayoutPageNavbar class="h-[64px] max-h-[64px]" />
+      <LayoutPageNavbar class="h-[52px] max-h-[52px]" />
       <LayoutPageContent class="max-w-5xl m-auto h-full">
         <slot />
       </LayoutPageContent>
