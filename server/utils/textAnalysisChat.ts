f425a6e27c8e9d4f29817fdeba106e8489cad0dd
mikechao <mike.chao.one@gmail.com>
2025-01-03 13:22:05 -0800

    Added ChatGoogleGenerativeAI model for textAnalysisChat

--- server/utils/textAnalysisChat.ts
+++ server/utils/textAnalysisChat.ts
@@ -4,13 +4,21 @@ import { BufferMemory } from "langchain/memory";
 import { ConversationChain } from "langchain/chains"
 import { getFirestoreChatMessageHistory } from "./firestoreChatHistory";
 import type { LLMResult } from "@langchain/core/outputs";
+import { AIModel } from "~/types/enums";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
 
 const runtimeConfig = useRuntimeConfig()
 
-const llm = new ChatOpenAI({
+const gpt4oMini = new ChatOpenAI({
   model: 'gpt-4o-mini',
   temperature: 0,
   apiKey: runtimeConfig.openaiAPIKey,
+})
+
+const geminiPro = new ChatGoogleGenerativeAI({
+  model: 'gemini-1.5-pro',
+  temperature: 0,
+  apiKey: runtimeConfig.googleAPIKey,
 })
 
 export async function textAnalysis(textChatRequest: TextChatRequest, uid: string, prompt: PromptTemplate): Promise<TextChatResposne> {
@@ -32,6 +40,7 @@ export async function textAnalysis(textChatRequest: TextChatRequest, uid: string
     },
   }
   console.log('Using AI Model', textChatRequest.aiModel)
+  const llm = textChatRequest.aiModel === AIModel.GPT_4o_mini ? gpt4oMini : geminiPro;
   const chain = new ConversationChain({ llm: llm, prompt: prompt, memory: memory, })
   const inputs = textChatRequest.messages
   const joined = inputs.join('\n')
