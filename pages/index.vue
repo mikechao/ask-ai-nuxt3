d73e50eaaac910447b177829274e9d1dcce3b25a
mikechao <mike.chao.one@gmail.com>
2025-01-05 19:32:13 -0800

    Better mobile layout for index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,25 +6,28 @@ const LayoutPageWrapper = defineAsyncComponent(() => import('~/layers/nuxt-aweso
 <template>
   <LayoutPageWrapper class="flex-1 flex">
     <LayoutPageSection class="flex-1 flex">
-      <div class="flex-1 flex flex-col items-center justify-center">
+      <div class="flex-1 flex flex-col items-center justify-center ">
         <AwesomeButton
           size="lg"
-          text="Ask questions about written text"
           to="/text"
-          class="font-extrabold mb-4"
-        />
+          class="font-extrabold mb-4 max-sm:w-full max-sm:text-center max-sm:px-4 text-center"
+        >
+          <span class="max-sm:text-base">Ask questions about written text</span>
+        </AwesomeButton>
         <AwesomeButton
           size="lg"
-          text="Ask questions about spoken language"
           to="/audio"
-          class="font-extrabold mb-4"
-        />
+          class="font-extrabold mb-4 max-sm:w-full max-sm:text-center max-sm:px-4 text-center"
+        >
+          <span class="max-sm:text-base">Ask questions about spoken language</span>
+        </AwesomeButton>
         <AwesomeButton
           size="lg"
-          text="Ask questions about an image"
           to="/image"
-          class="font-extrabold"
-        />
+          class="font-extrabold max-sm:w-full max-sm:text-center max-sm:px-4 text-center"
+        >
+          <span class="max-sm:text-base">Ask questions about an image</span>
+        </AwesomeButton>
       </div>
     </LayoutPageSection>
   </LayoutPageWrapper>
