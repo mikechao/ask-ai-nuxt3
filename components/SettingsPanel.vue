01456d16123788bf2801bd3377144911b43145ac
mikechao <mike.chao.one@gmail.com>
2025-01-06 13:23:26 -0800

    Fix issue where arrow-down was not shown when max-sm is met

--- components/SettingsPanel.vue
+++ components/SettingsPanel.vue
@@ -2,30 +2,39 @@
 import { VueSidePanel } from 'vue3-side-panel'
 import 'vue3-side-panel/dist/vue3-side-panel.css'
 import { useSettingStore } from '~/stores/settingsStore'
-
 const isOpen = ref(false)
 const settingsStore = useSettingStore()
 const sidePanelClass = 'bg-[#f8f9fa] dark:bg-gray-800'
 const isMaxSm = ref(false)
 const isVertical = ref(true)
+
+// button styles
 const verticalStyle = 'absolute left-0 top-1/2 transform -translate-y-1/2 ml-2'
 const horizontalStyle = 'absolute top-0 left-1/2 transform -translate-x-1/2 mt-8'
 const buttonStyle = ref(verticalStyle)
+
+// the arrow icons to use
 const arrowRight = 'mdi:arrow-expand-right'
 const arrowDown = 'mdi:arrow-expand-down'
 const arrowIcon = ref(arrowRight)
 
+// styles for first arrow
 const firstArrowVerticalStyle = 'mb-2'
 const firstArrowHorizontalStyle = 'mr-2'
 const firstArrowStyle = ref(firstArrowVerticalStyle)
 
+// styles for second arrow
 const secondArrowVerticalStyle = 'mt-2'
 const secondArrowHorizontalStyle = 'ml-2'
 const secondArrowStyle = ref(secondArrowVerticalStyle)
 
+// button sizes
 const buttonVerticalSize = 'md'
 const buttonHorizontalSize = 'xs'
 const buttonSize = ref(buttonVerticalSize)
+
+// a key for the button to trigger redraw
+const buttonKey = ref(newKeyValue())
 
 watch(isMaxSm, (value) => {
   if (value) {
@@ -35,6 +44,7 @@ watch(isMaxSm, (value) => {
     firstArrowStyle.value = firstArrowHorizontalStyle
     secondArrowStyle.value = secondArrowHorizontalStyle
     buttonSize.value = buttonHorizontalSize
+    buttonKey.value = newKeyValue()
   } else {
     isVertical.value = true
     buttonStyle.value = verticalStyle
@@ -42,12 +52,17 @@ watch(isMaxSm, (value) => {
     firstArrowStyle.value = firstArrowVerticalStyle
     secondArrowStyle.value = secondArrowVerticalStyle
     buttonSize.value = buttonVerticalSize
+    buttonKey.value = newKeyValue()
   }
 })
 
 const checkMaxSmBreakpoint = () => {
   const mediaQuery = window.matchMedia('(max-width: 639px)')
   isMaxSm.value = mediaQuery.matches
+}
+
+function newKeyValue() {
+  return Math.random() * 10000
 }
 
 onMounted(() => {
@@ -67,6 +82,7 @@ async function aiModeHandler({checked, target}: {checked: boolean, target: HTMLI
 </script>
 <template>
   <AwesomeButton
+    :key="buttonKey"
     :size="buttonSize"
     :vertical="isVertical" 
     :class="buttonStyle"
