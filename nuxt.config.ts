7afe1153f4c9a7e2d28dead0f03364f380fd407c
mikechao <mike.chao.one@gmail.com>
2025-01-08 22:05:31 -0800

    prerender / and /login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  routeRules: {
+    '/': {prerender: true},
+    '/login': {prerender: true}
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/app.css'],
