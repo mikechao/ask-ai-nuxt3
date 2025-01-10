b0624b36d9d67faa82658a813bbac338ebf11d8b
mikechao <mike.chao.one@gmail.com>
2025-01-09 17:01:07 -0800

    Set ssr back to false, was casuing issues on netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  routeRules: {
-    '/': {prerender: true},
-    '/login': {prerender: true}
-  },
+  ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/app.css'],
